ba9ab33076bff111dc5d3d55bbe1da74df0471dd
Vita Dovhan <87687287+Vita-Dovhan@users.noreply.github.com>
2022-01-20 13:43:23 +0200

    add function clear

--- src/index.js
+++ src/index.js
@@ -6,56 +6,60 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-  searchEl: document.querySelector('.search-form'),
+  searchImages: document.querySelector('.search-form'),
   loudMoreBtn: document.querySelector('.load-more'),
-  card: document.querySelector('.gallery'),
+  cardsGallery: document.querySelector('.gallery'),
 };
 const newsApiService = new NewsApiService();
 
-refs.searchEl.addEventListener('submit', onSearch);
+refs.searchImages.addEventListener('submit', onSearch);
 refs.loudMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
   newsApiService.query = e.currentTarget.elements.searchQuery.value;
+  if (!newsApiService.query) {
+    Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    refs.cardsGallery.insertAdjacentHTML = ''
+  }
+  clearHitsContainer();
   newsApiService.resetPage();
-  newsApiService.fetchHits().then(render);
-
+  newsApiService.fetchHits().then(renderCardsGallery);
+  refs.searchImages.reset();
 }
 function onLoadMore() {
-  newsApiService.fetchHits().then(render);
+  newsApiService.fetchHits().then(renderCardsGallery);
 }
-
-
-function render(hits) {
-
+function renderCardsGallery(hits) {
   const marcup = hits
-    .map(element => {
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
       return `
        <div class="photo-card">
-  <img src="${element.webformatURL}" width ="310" height="160" alt="${element.tags}" loading="lazy" />
+  <img src="${webformatURL}" width ="310" height="160" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b> ${element.likes}
+      <b>Likes</b> ${likes}
     </p>
     <p class="info-item">
-      <b>Views</b> ${element.views}
+      <b>Views</b> ${views}
     </p>
     <p class="info-item">
-      <b>Comments</b> ${element.comments}
+      <b>Comments</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b> ${element.downloads}
+      <b>Downloads</b> ${downloads}
     </p>
   </div>
 </div>`
     })
     .join();
-  refs.card.insertAdjacentHTML('beforeend', marcup);
+  refs.cardsGallery.insertAdjacentHTML('beforeend', marcup);
+}
+function clearHitsContainer() {
+  refs.cardsGallery.innerHTML = '';
 }
 
+// const gallery = new SimpleLightbox('.photo-card div');
+// gallery.on('show.simplelightbox', function () {
 
-const gallery = new SimpleLightbox('.gallery div');
-gallery.on('show.simplelightbox', function () {
-
-});
+// });
--- src/js/news-service.js
+++ src/js/news-service.js
@@ -2,26 +2,19 @@ export default class newsApiService {
   constructor() {
     this.form = '';
     this.page = 1;
-
   }
   fetchHits() {
     return fetch(`https://pixabay.com/api/?key=25313691-12b0cc3e1565a2c76ff5ddf8f&q=${this.form}
     &lang=en&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
-      .then(r => r.json())
-      .then(data => {
-        console.log(data)
-        this.incrementPage();
-        return data.hits;
+      .then(responce => responce.json())
+      .then(({ hits }) => {
+        this.page += 1;
+        return hits;
       });
   }
-  incrementPage() {
-    this.page += 1;
-  }
-
   resetPage() {
     this.page = 1;
   }
-
   get query() {
     return this.form;
   }
